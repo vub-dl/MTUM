8c932cfd00b82f98e7cf01c69b17f235a0c167e8
Diego Olaya <42603773+daolayal@users.noreply.github.com>
2020-01-28 14:39:16 +0100

    Create main.R

---
+++ main.R
@@ -0,0 +1,3 @@
+# Load packages
+packages <- list("caret","splitstackshape","scales","corrplot","AppliedPredictiveModeling","ggplot2","uplift","plyr","ggpubr","moments","stringr","sas7bdat","randomForest","nnet","twang","RItools","RItools","tableone","MatchIt","car","reshape2","ggthemes","rowr")
+lapply(packages, require, character.only = TRUE)
